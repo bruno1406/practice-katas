4a39f505d3b466212e75f2ffe4b82ca8d6027665
Bruno Barbagelata <bruno140685@gmail.com>
2021-09-22 10:44:55 -0400

    reverseWords 8kyu

--- 8kyu.js
+++ 8kyu.js
@@ -39,3 +39,8 @@ function noSpace(x) {
 function multipleOfIndex(array) {
   return array.filter((num, i) => num % i === 0);
 }
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+function reverseWords(str) {
+  return str.split(" ").reverse().join(" ");
+}
