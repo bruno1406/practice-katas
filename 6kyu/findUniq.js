972fd23d0decd76631e39ae3006f506bf03f286c
Bruno Barbagelata <bruno140685@gmail.com>
2021-10-06 09:16:34 -0400

    findUniq.js

---
+++ 6kyu/findUniq.js
@@ -0,0 +1,10 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+
+function findUniq(arr) {
+  return Number(
+    arr.filter((num) => arr.indexOf(num) === arr.lastIndexOf(num)).join("")
+  );
+}
