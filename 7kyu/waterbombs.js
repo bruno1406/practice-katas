384bd8548e1b90623431aa154485710c2133ee4e
Bruno Barbagelata <bruno140685@gmail.com>
2021-10-16 09:56:25 -0400

    7kyu waterbombs.js

--- 7kyu/waterbombs.js
+++ 7kyu/waterbombs.js
@@ -29,3 +29,9 @@ function waterbombs(fire, w) {
   }
   return sum;
 }
+
+//oneliner
+
+// function waterbombs(fire, w) {
+//     return fire.split("Y").reduce((a, b) => a + Math.ceil(b.length / w), 0)
+//   }
