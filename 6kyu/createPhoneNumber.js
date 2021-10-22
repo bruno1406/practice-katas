f000dfc0e262dce1d9e14fd17d37bed6d0caf520
Bruno Barbagelata <bruno140685@gmail.com>
2021-10-22 13:07:45 -0400

    6kyu createPhoneNumbers.js

--- 6kyu/createPhoneNumber.js
+++ 6kyu/createPhoneNumber.js
@@ -8,3 +8,13 @@
 function createPhoneNumber(numbers) {
   return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
 }
+
+//better code:
+// function createPhoneNumber(numbers){
+//     let format = "(xxx) xxx-xxxx";
+
+//     for(let i = 0; i < numbers.length; i++){
+//       format = format.replace('x', numbers[i]);
+//     }
+//     return format;
+//   }
