be737c0195e477c50617e42631c1e0e75936ff63
Bruno Barbagelata <bruno140685@gmail.com>
2021-10-27 10:28:22 -0400

    8kyu squareArea.js

---
+++ 8kyu/squareArea.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/5748838ce2fab90b86001b1a/train/javascript
+
+// Complete the function that calculates the area of the red square,
+//when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+function squareArea(A) {
+  return Number(Math.pow((A * 4) / (2 * Math.PI), 2).toFixed(2));
+}
