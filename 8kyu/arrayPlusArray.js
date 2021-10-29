bbe8461a0de5e41e434306da0ce1fb0a71f08d76
Bruno Barbagelata <bruno140685@gmail.com>
2021-10-29 10:15:56 -0400

    8kyu arrayPlusArray.js

---
+++ 8kyu/arrayPlusArray.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/javascript
+
+// I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+function arrayPlusArray(arr1, arr2) {
+  return (
+    arr1.reduce((acc, curr) => acc + curr, 0) +
+    arr2.reduce((acc, curr) => acc + curr, 0)
+  ); //something went wrong
+}
