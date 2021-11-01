427f634f33e93126b5205dacf270bbcd947dae38
Bruno Barbagelata <bruno140685@gmail.com>
2021-11-01 09:03:23 -0400

    6kyu rotate.js

---
+++ 6kyu/rotate.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/586560a639c5ab3a260000f3/train/javascript
+
+// Create a function named rotate() that accepts a string argument and
+//returns an array of strings with each letter from the input string being rotated to the end.
+
+// rotate("Hello") // => ["elloH", "lloHe", "loHel", "oHell", "Hello"]
