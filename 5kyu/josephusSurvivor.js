9f9f3baeda63309d51d4f4662c0dd046340f1904
Bruno Barbagelata <bruno140685@gmail.com>
2021-11-02 11:10:50 -0400

    5kyu josephusSurvivor.js

--- 5kyu/josephusSurvivor.js
+++ 5kyu/josephusSurvivor.js
@@ -31,3 +31,8 @@ function josephusSurvivor(n, k) {
 
   return arr[arr.length - 1];
 }
+
+//one liner
+// function josephusSurvivor(n,k){
+//     return n < 1 ? 1 : (josephusSurvivor(n - 1, k) + --k) % n + 1;
+// }
