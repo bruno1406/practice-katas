562e8d6bcc7e875b993c514c72cb24e8e3b80031
Bruno Barbagelata <bruno140685@gmail.com>
2021-11-02 10:12:03 -0400

    6kyu f.js

--- 6kyu/f.js
+++ 6kyu/f.js
@@ -11,3 +11,18 @@ function f(s) {
   let m = s.match(/^(.*?)\1+$/);
   return m ? [m[1], s.length / m[1].length] : [s, 1];
 }
+
+//other one
+
+// function f(s) {
+//     let sequence = '';
+//   let reg = new RegExp(`${sequence}`, 'g');
+//   let i = 0;
+//   while(s.match(reg).join('') !== s && i < s.length) {
+//     sequence += s[i];
+//     reg = new RegExp(`${sequence}`, 'g');
+//     i++;
+//   }
+
+//   return [s.match(reg)[0], s.match(reg).length]
+// }
