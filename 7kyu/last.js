0e3a83c4dec5348b9684197016b0b2e664ee968c
Bruno Barbagelata <bruno140685@gmail.com>
2021-11-08 20:06:42 -0500

    7kyu last.js

---
+++ 7kyu/last.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/57eba158e8ca2c8aba0002a0/train/javascript
+
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
