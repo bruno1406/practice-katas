0264dfb03e324080a81c252700fa7b57a6a0af05
Bruno Barbagelata <bruno140685@gmail.com>
2021-11-08 20:07:05 -0500

    7kyu last.js

--- 7kyu/last.js
+++ 7kyu/last.js
@@ -5,3 +5,9 @@
 // If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
 
 // All inputs will be valid.
+
+function last(x) {
+  return x
+    .split(" ")
+    .sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
+}
