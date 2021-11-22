a81298b12b0c77f8156c79019fdfdeff22a4f4f8
Bruno Barbagelata <bruno140685@gmail.com>
2021-11-21 19:34:58 -0500

    8kyu howMuchILoveYou.js

--- 8kyu/howMuchILoveYou.js
+++ 8kyu/howMuchILoveYou.js
@@ -11,3 +11,15 @@
 // When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
 
 // Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+
+function howMuchILoveYou(nbPetals) {
+  let arr = [
+    "I love you",
+    "a little",
+    "a lot",
+    "passionately",
+    "madly",
+    "not at all",
+  ];
+  return arr[(nbPetals - 1) % arr.length];
+}
