de1dfd0b5e0c45a9ed199d03438fbc15a6fbccba
Bruno Barbagelata <bruno140685@gmail.com>
2021-12-01 10:51:53 -0500

    7kyu bump.js

--- 7kyu/bump.js
+++ 7kyu/bump.js
@@ -6,3 +6,7 @@
 // Unfortunately for you, your drive is very bumpy!
 //Given a string showing either flat road ("_") or bumps ("n"),
 //work out if you make it home safely. 15 bumps or under, return "Woohoo!", over 15 bumps return "Car Dead".
+
+function bump(x) {
+  return (x.match(/n/g) || []).length <= 15 ? "Woohoo!" : "Car Dead";
+}
