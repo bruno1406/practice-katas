2bd5fc4efa1f596d56ba35ffad753df58f6a353c
Bruno Barbagelata <bruno140685@gmail.com>
2021-12-06 23:01:31 -0500

    8kyu remainder.js

--- 8kyu/remainder.js
+++ 8kyu/remainder.js
@@ -3,3 +3,11 @@
 // Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
 
 // Division by zero should return NaN.
+
+function remainder(a, b) {
+  if (a > b) {
+    return a % b;
+  } else {
+    return b % a;
+  }
+}
