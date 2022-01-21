92f89b4f8a03afd1558e5eea4068bb8ae2905aa2
Bruno Barbagelata <bruno140685@gmail.com>
2022-01-21 13:17:26 -0500

    7kyu tennisGamePoints.js

--- 7kyu/tennisGamePoints.js
+++ 7kyu/tennisGamePoints.js
@@ -1,1 +1,19 @@
 //https://www.codewars.com/kata/590942d4efde93886900185a/train/javascript
+
+// Input/Output
+// [input] string score
+
+// A string in the format <p1>-<p2> representing a valid score, where <p1> is the first player's score, and <p2> is the second player's score.
+
+// [output] an integer
+
+// The number of points won so far.
+
+// Example
+// For score = "15-40", the output should be 4.
+
+// The first player won 1 point, and the second 3, so 1 + 3 = 4 points have been won.
+
+// For score = "30-all", the output should be 4.
+
+// The players have won 2 points each.
