e5f4bcd969d6c5dd067a96ac3a4b51ab781518ce
Bruno Barbagelata <bruno140685@gmail.com>
2022-05-03 11:36:21 -0400

    7kyu bingo.js

--- 7kyu/bingo.js
+++ 7kyu/bingo.js
@@ -7,3 +7,13 @@
 //Write a function where you will win the game if your numbers can spell "BINGO".
 //They do not need to be in the right order in the input array). Otherwise you will lose.
 //Your outputs should be "WIN" or "LOSE" respectively.
+
+function bingo(a) {
+  const bingo = [2, 7, 9, 14, 15];
+
+  const isItBingo = bingo.every((value) => {
+    return a.includes(value);
+  });
+
+  return isItBingo ? "WIN" : "LOSE";
+}
