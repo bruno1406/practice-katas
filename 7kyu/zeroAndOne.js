32250e6e8661efc759ea7113d8ad3048876240cd
Bruno Barbagelata <bruno140685@gmail.com>
2022-05-04 15:59:15 -0400

    7kyu zeroAndOne.js

---
+++ 7kyu/zeroAndOne.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/58ad09d6154165a1c80000d1/train/javascript
+
+// You have a string that consists of zeroes and ones. Now choose any two adjacent positions in the string: if one of them is 0, and the other one is 1, remove these two digits from the string.
+
+// Return the length of the resulting (smallest) string that you can get after applying this operation multiple times?
+
+// Note: after each operation, the remaining digits are separated by spaces and thus not adjacent anymore - see the examples below.
+
+// Examples
+// For "01010" the result should be 1:
+
+// "01010"  -->  "  010"  -->  "    0"
+// For "110100" the result should be 2:
+
+// "110100"  -->  "1  100"  -->  "1    0"
