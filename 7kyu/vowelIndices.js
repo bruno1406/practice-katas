6af5de9b50030b5fe11b8e52e7efb49c0ff5ce31
Bruno Barbagelata <bruno140685@gmail.com>
2022-05-05 10:07:12 -0400

    vowelIndices.js 7kyu

--- 7kyu/vowelIndices.js
+++ 7kyu/vowelIndices.js
@@ -1,1 +1,12 @@
 //https://www.codewars.com/kata/5680781b6b7c2be860000036/train/javascript
+
+// We want to know the index of the vowels in a given word, for example,
+// there are two vowels in the word super (the second and fourth letters).
+
+// So given a string "super", we should return a list of [2, 4].
+
+// Some examples:
+// Mmmm  => []
+// Super => [2,4]
+// Apple => [1,5]
+// YoMama -> [1,2,4,6]
