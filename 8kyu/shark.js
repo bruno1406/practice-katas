b4d9e9518253433476d68c42b6a89dfd20e14600
Bruno Barbagelata <bruno140685@gmail.com>
2022-05-05 09:48:29 -0400

    shark.js 8kyu

--- 8kyu/shark.js
+++ 8kyu/shark.js
@@ -15,3 +15,10 @@
 // dolphin = a boolean, if true, you can half the swimming speed of the shark as the dolphin will attack it.
 // The pontoon, you, and the shark are all aligned in one dimension.
 // If you make it, return "Alive!", if not, return "Shark Bait!".
+
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) {
+  dolphin ? (sharkSpeed /= 2) : null;
+  return pontoonDistance / youSpeed < sharkDistance / sharkSpeed
+    ? "Alive!"
+    : "Shark Bait!";
+}
