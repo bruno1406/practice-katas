8068cd236934e7ea748695a8e6b59db57e6e50f9
Bruno Barbagelata <bruno140685@gmail.com>
2022-05-09 12:15:26 -0400

    dontGiveMeFive.js

--- 7kyu/dontGiveMeFive.js
+++ 7kyu/dontGiveMeFive.js
@@ -34,3 +34,15 @@ function dontGiveMeFive(start, end) {
   }
   return count;
 }
+
+//Best solution:
+
+function dontGiveMeFive(start, end) {
+  let count = 0;
+  for (let i = start; i <= end; i++) {
+    if (!/5/.test(i)) {
+      count++;
+    }
+  }
+  return count;
+}
