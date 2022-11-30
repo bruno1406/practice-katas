f2d5d3621c30d7635efb34f34d3fb334f7628c31
Bruno Barbagelata <bruno140685@gmail.com>
2022-11-30 11:15:08 -0500

    pythagorean Triple 8kyu

--- 8kyu/pithagoreanTriple.js
+++ 8kyu/pithagoreanTriple.js
@@ -27,3 +27,10 @@ function isPythagoreanTriple(integers) {
     ? true
     : false;
 }
+
+//Best Answer:
+
+// function isPythagoreanTriple(integers) {
+//     let massiv = integers.sort((a, b) => a - b)
+//     return (massiv[0] ** 2 + massiv[1] ** 2 == massiv[2] ** 2)
+//   }
