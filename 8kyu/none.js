f9878f77af48154221a75b596f059e8a3b18485b
Bruno Barbagelata <bruno140685@gmail.com>
2022-11-30 11:44:17 -0500

    pythagorean Triple 8kyu

--- 8kyu/none.js
+++ 8kyu/none.js
@@ -1,2 +1,7 @@
 // Create a function none? that accepts an array and a function, and returns true
 // if the function returns true for none of the items in the array. An empty list should return true.
+
+function none(arr, fun) {
+  let result = arr.filter((item) => fun(item) === true);
+  return result.length > 0 ? false : true;
+}
