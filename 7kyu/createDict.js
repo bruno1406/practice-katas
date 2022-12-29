f40695cb5ed469c85f017b0aa98cd63de50abbfa
Bruno Barbagelata <bruno140685@gmail.com>
2022-12-29 09:44:15 -0500

    7kyu createDict.js

---
+++ 7kyu/createDict.js
@@ -0,0 +1,4 @@
+// There are two lists, possibly of different lengths. The first one consists of keys,
+// the second one consists of values. Write a function createDict(keys, values)
+// that returns a dictionary created from keys and values. If there are not enough values,
+// the rest of keys should have a None (JS null)value. If there not enough keys, just ignore the rest of values.
