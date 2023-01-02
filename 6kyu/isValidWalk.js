637c75ea097086b9eb86920fbd9c58a646d1f829
Bruno Barbagelata <bruno140685@gmail.com>
2023-01-02 01:24:33 -0500

    6kyu isValidWalk.js

--- 6kyu/isValidWalk.js
+++ 6kyu/isValidWalk.js
@@ -11,3 +11,23 @@
 // Note: you will always receive a valid array containing a random assortment of
 //direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty
 // array (that's not a walk, that's standing still!).
+
+function isValidWalk(walk) {
+  if (walk.length !== 10) {
+    return false;
+  }
+  let north = 0;
+  let west = 0;
+  for (let i = 0; i < walk.length; i++) {
+    if (walk[i] === "n") {
+      north++;
+    } else if (walk[i] === "s") {
+      north--;
+    } else if (walk[i] === "w") {
+      west++;
+    } else {
+      west--;
+    }
+  }
+  return north === 0 && west === 0 ? true : false;
+}
