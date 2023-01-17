284b9b40d90cc053d0d29d23f3ae090b97f0885f
Bruno Barbagelata <bruno140685@gmail.com>
2023-01-17 11:03:35 -0500

    5kyu number9.js

--- 5kyu/number9.js
+++ 5kyu/number9.js
@@ -1,3 +1,18 @@
+// It's 9 time!
+
+// I want to count from 1 to n. How many times will I use a '9'?
+
+// 9, 19, 91.. will contribute one '9' each, 99, 199, 919.. wil contribute two '9's each...etc
+
+// Note: n will always be a positive integer.
+
+// Examples (input -> output)
+// 8  -> 0
+// 9  -> 1
+// 10 -> 1
+// 98 -> 18
+// 100 -> 20
+
 function number9(n) {
   var count = 0;
   for (var i = 1; i <= n; i *= 10) {
